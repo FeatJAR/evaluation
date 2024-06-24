b13da8b330ea74b9c2021b888f30e35b694abcc6
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-06-24 19:18:21 +0200

    Adds max memory option to available CLI options

--- src/main/java/de/featjar/evaluation/Evaluator.java
+++ src/main/java/de/featjar/evaluation/Evaluator.java
@@ -102,6 +102,7 @@ public abstract class Evaluator implements ICommand {
                 modelsPathOption,
                 resourcesPathOption,
                 timeout,
+                memory,
                 randomSeed,
                 overwrite,
                 systemsOption,
